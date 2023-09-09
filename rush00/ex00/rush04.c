2b78e73310180b800526a0c2ee5ecf10f75cc587
Simone Ferraresi <sferrare@c1r3p11.42firenze.it>
2023-09-09 19:10:41 +0200

    rush00 new

---
+++ rush00/ex00/rush04.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush04.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sferrare <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/09 17:24:10 by sferrare          #+#    #+#             */
+/*   Updated: 2023/09/09 17:26:07 by sferrare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	rush(int x, int y)
+{
+	int	row;
+	int	col;
+
+	if (x <= 0 || y <= 0)
+		return ;
+	row = 1;
+	while (row <= y)
+	{
+		col = 1;
+		while (col <= x)
+		{
+			if ((row == 1 && col == 1) || (row == y && col == x))
+				ft_putchar('A');
+			else if ((row == 1 && col == x) || (row == y && col == 1))
+				ft_putchar('C');
+			else if (row == 1 || row == y || col == 1 || col == x)
+				ft_putchar('B');
+			else
+				ft_putchar(' ');
+			col++;
+		}
+		ft_putchar('\n');
+		row++;
+	}
+}
